8207a27c52294880af577826dc966ce2854b438e
CristinaDAmico <crydamico94@gmail.com>
2021-05-17 16:42:56 +0200

    points 1-2

--- index.php
+++ index.php
@@ -9,5 +9,26 @@
 
 <body>
     
+    <?php
+    // CODICE PHP
+
+    // 1. Creare una variabile con assegnato un testo per popolare un paragrafo.
+    $text = " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita modi provident ea molestiae nesciunt sequi ullam perferendis, eum corrupti ut quasi consequuntur tempora maiores enim porro iure est nobis eos! ";
+
+    ?>
+
+    <header>
+        <h1>Welcome!</h1>
+    </header>
+
+    <main>
+        <!-- 2. Visualizzare a schermo il paragrafo -->
+        <p> <?php echo $text; ?> </p>
+    </main>
+
+
+
+
+
 </body>
 </html>
